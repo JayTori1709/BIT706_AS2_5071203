39c4a76c272f53ee58e948b2d6390c74150d4d0d
JayTori1709 <jaydenleviwood@outlook.co.nz>
2025-05-08 23:04:59 +1200

    added changes to the ui

--- Assessment2.App/CustomerEditorWindow.xaml.cs
+++ Assessment2.App/CustomerEditorWindow.xaml.cs
@@ -1,18 +1,17 @@
-﻿// CustomerEditorWindow.xaml.cs
-using System.Windows;
-using Assessment2.App.BusinessLayer;
+﻿using System.Windows;
+using Assessment2.App.BusinessLayer; 
 
 namespace Assessment2.App.BusinessLayer
 {
     public partial class CustomerEditorWindow : Window
     {
-        public Customer? Customer { get; private set; } // Made nullable
+        public Customer Customer { get; private set; }
 
         public CustomerEditorWindow(Customer? customer = null)
         {
             InitializeComponent();
             Customer = customer ?? new Customer();
-            DataContext = Customer; // Set DataContext for binding
+            DataContext = Customer;
         }
 
         private void OnSave(object sender, RoutedEventArgs e)
