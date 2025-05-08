9144a0b8e04c10bd460edd0ad1050c00e27a7c2e
JayTori1709 <jaydenleviwood@outlook.co.nz>
2025-05-08 23:06:28 +1200

    initialising commit

--- Assessment2.App/obj/Debug/net7.0-windows/Assessment2.App.AssemblyInfo.cs
+++ Assessment2.App/obj/Debug/net7.0-windows/Assessment2.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assessment2.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38c1f3e53527081aa0ea095d10882f509d52b9a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39c4a76c272f53ee58e948b2d6390c74150d4d0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assessment2.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assessment2.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
