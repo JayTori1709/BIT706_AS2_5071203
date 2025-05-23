a338639071543d4226053493327d77660db4d1e1
JayTori1709 <jaydenleviwood@outlook.co.nz>
2025-05-23 22:22:46 +1200

    program works, fixing one exe error

--- Assessment2.Tests/obj/Debug/net8.0-windows/Assessment2.Tests.AssemblyInfo.cs
+++ Assessment2.Tests/obj/Debug/net8.0-windows/Assessment2.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assessment2.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7476e048592b3f45317636d07c88f7bf58f9b7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eacf93a0392333fc9fed6dfc400fd441d281c840")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assessment2.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assessment2.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
